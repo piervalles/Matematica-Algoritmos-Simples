a41de1669f3fd4966f63b2a559649a4d9efacb52
Piero Valles <200243539+piervalles@users.noreply.github.com>
2025-03-25 15:20:56 -0500

    Revert "Merge branch 'dev'"
    This reverts commit 6315430c205646a56929e50bf9545e1a5de3329e, reversing
    changes made to 0ced4969c95e84c2ba21971d30be20ba4d628e5a.

--- metodo-biseccion/src/main/java/com/example/Intervalos.java
+++ metodo-biseccion/src/main/java/com/example/Intervalos.java
@@ -1,71 +1,18 @@
 package com.example;
-import com.ezylang.evalex.Expression;
-import com.ezylang.evalex.data.EvaluationValue;
 
 public class Intervalos {
     private double a;
     private double b;
-    private String fxString;
 
     public Intervalos (Variables datos){
         this.a = datos.getA();
         this.b = datos.getB();
-        this.fxString = datos.getFxString();
+        //this.fxString = datos.getFxString();
     }
 
     public boolean  reglaIntervalos (){
-        Double fa=null;
-        Double fb=null;
-
-        try {
-            Expression expression = new Expression(fxString);
-
-            EvaluationValue eFa = expression.with("x", a).evaluate();
-            fa = eFa.getNumberValue().doubleValue();
-
-            EvaluationValue eFb = expression.with("x", b).evaluate();
-            fb = eFb.getNumberValue().doubleValue();   
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-        
-        if (a>=b || fa*fb==0) {
-            if (a>=b) {
-                System.out.println("Error: \'a\' debe ser menor que \'b\'");
-            } else {
-                if (fa*fb==0) {
-                    if (fa==0 && fb==0) {
-                        System.out.println("Las raices de la función son:");
-                        if ((a == Math.floor(a)) && (b == Math.floor(b))) {
-                            // Es un número entero (sin decimales significativos)
-                            System.out.println("x1 ="+(int)a);
-                            System.out.println("x2 ="+(int)b);
-                        } else {
-                            // Tiene decimales significativos
-                            System.out.println("x1 = "+a);
-                            System.out.println("x2 = "+b);
-                        }
-                    } else {
-                        if (fa==0) {
-                            System.out.println("Se encontro una raiz en: ");
-                            if (a==Math.floor(a)) {
-                                System.out.println("x = "+(int)a);
-                            } else {
-                                // Tiene decimales significativos
-                                 System.out.println("x = "+a);
-                            }
-                        } else {
-                            System.out.println("Se encontro una raiz en: ");
-                            if (b==Math.floor(b)) {
-                                System.out.println("x = "+(int)b);
-                            } else {
-                                // Tiene decimales significativos
-                                 System.out.println("x = "+b);
-                            }
-                        }
-                    }
-                }     
-            }
+        if (a>=b) {
+            System.out.println("Error: \'a\' debe ser menor que \'b\'");
             return false;
         } else {
             return true;
